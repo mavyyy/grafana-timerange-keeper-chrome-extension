78061c9ddfaf7c1b20a148e15fe2763542b356af
mavyyy <shun.mab.37+github@gmail.com>
2020-04-23 04:12:10 +0900

    replace current functionality by vue

--- popup.js
+++ popup.js
@@ -1,28 +1,56 @@
-var msg = {}
-var params = {}
-var tabId = null
-
 function epochToStr(epoch){
   return new Date(parseInt(epoch)).toLocaleString()
 }
+var tabId = null;
+
+
+const app = new Vue({
+  el:"#app",
+  data:{
+    msg:{},
+    params:{
+      current_from:"",
+      current_to:"",
+      configured_from:"",
+      configured_to:""
+    }
+  },
+  computed:{
+    current_from:function(){
+      return epochToStr(this.params.current_from);
+    },
+    current_to:function(){
+      return epochToStr(this.params.current_to);
+    },
+    configured_from:function(){
+      return epochToStr(this.params.configured_from);
+    },
+    configured_to:function(){
+      return epochToStr(this.params.configured_to);
+    }
+  }
+})
+
+
 
 function setCongifuredTimeRange(e) {
   params.configured_from = this.from;
   params.configured_to = this.to;
   var storage_obj = {};
-  storage_obj[msg.hostname] = params
-  document.getElementById("configured_from_locale").innerText=epochToStr(this.from)
-  document.getElementById("configured_to_locale").innerText=epochToStr(this.to)
+  storage_obj[app.msg.hostname] = params
+  app.params.configured_from=this.from;
+  app.params.configured_to=this.to;
   chrome.storage.local.set(storage_obj)
 }
+
 
 function applyConfiguredTimeRange(e) {
   params.current_from = this.from;
   params.current_to = this.to;
   var storage_obj = {};
-  storage_obj[msg.hostname] = params;
-  document.getElementById("current_from_locale").innerText=epochToStr(this.from)
-  document.getElementById("current_to_locale").innerText=epochToStr(this.to)
+  storage_obj[app.msg.hostname] = params;
+  app.params.current_from = this.from;
+  app.params.current_to = this.to;
   chrome.tabs.sendMessage(tabId, {
     type: "apply",
     params: {
@@ -40,7 +68,7 @@ chrome.tabs.query({
   chrome.tabs.sendMessage(tabId, {
     type: "popup"
   }, function (response) {
-    msg = response;
+    app.msg = response;
     if (!response.isGrafanaWindow) {
       return;
     }
@@ -56,7 +84,7 @@ chrome.tabs.query({
         ];
         for (let key of keys) {
           if (params[key]) {
-            document.getElementById(key + "_locale").innerText = epochToStr(params[key]);
+            app.params[key] = params[key];
           }
         }
       }
